3d551ff972879e19e2efa11121abba876ff8619e
iLazlow <karr54@hotmail.de>
2022-07-20 08:19:28 +0200

    -fixed port forwarding with express an websocket

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const moment = require('moment');
 const sqlite3 = require('sqlite3')
-const { WEB_PORT, WSS_PORT } = require('./config.json');
+const { PORT } = require('./config.json');
 const fs = require('fs');
 const cors = require("cors");
 const DAO = require('./dao');
 const express = require('express');
-const {WebSocketServer} = require('ws');
 const bodyParser = require('body-parser');
 
-const dao = new DAO('./main.db');
 const app = express();
+const dao = new DAO('./main.db');
+const wss = require('express-ws')(app);
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  //console.log('middleware');
+  //req.testing = 'testing';
+  return next();
+});
 
-const wss = new WebSocketServer({port: WSS_PORT});
-wss.on('connection', function connection(ws, req) {
-  const query = req.url.split('|')[1];
-  ws.publicKey = query;
+app.ws('/', function(ws, req) {
+  ws.publicKey = req.query.pub;
   
   dao.all("SELECT * FROM message_queue WHERE recipient = ? ORDER BY created ASC", [ws.publicKey]).then(result => {
     if(result.length > 0){
@@ -36,7 +40,7 @@ wss.on('connection', function connection(ws, req) {
     if(json.type == "msg"){
       console.log(`send message from ${json.sender} to ${json.recipient}`);
       var i = 0;
-      wss.clients.forEach(function(client) {
+      wss.getWss().clients.forEach(function(client) {
         if(client.publicKey == json.recipient){
           client.send(JSON.stringify({type: "message", msg: json.content, created: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}));
           i++;
@@ -52,14 +56,14 @@ wss.on('connection', function connection(ws, req) {
     }
   });
 
-  ws.send(JSON.stringify({type: "connected"}));
+  ws.send(JSON.stringify({type: "connected", key: ws.publicKey}));
 });
 
 /* Server Endpoints */
 app.get("/", (req, res) => res.send("Aces Server is working!"));
 
 app.post('/checkin', (req, res) => {
-  dao.get("SELECT * FROM accounts WHERE username LIKE '%?%'", [req.body.username]).then(result => {
+  dao.get("SELECT * FROM accounts WHERE username LIKE '%" + req.body.username + "%'").then(result => {
     console.log(result);
     if(result == undefined){
       res.send({status: "success", type: "CREATED", message: "Account with username " + req.body.username + " created"});
@@ -75,4 +79,8 @@ app.post('/checkin', (req, res) => {
   });
 });
 
-app.listen(WEB_PORT, () => console.log(`Aces Server listening on port ${WEB_PORT}!`));
+app.post('/check', (req, res) => {
+  res.send({status: "success", type: "CHECK", message: "Webserver check was successfully executed"});
+});
+
+app.listen(PORT, () => console.log(`Aces Server listening on port ${PORT}!`));
